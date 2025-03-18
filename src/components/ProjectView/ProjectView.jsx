5bf871e9b57ddcd16f4f9f583dc4c4cb25206ff9
MichaelSwain <michaeljswain@gmail.com>
2025-03-18 14:54:34 +0100

    add initial projectview markup and styling

--- src/components/ProjectView/ProjectView.jsx
+++ src/components/ProjectView/ProjectView.jsx
@@ -49,6 +49,32 @@ export const ProjectView = () => {
                 </div>
             </div>
             </div>
+
+            {/* table */}
+            <div className="project-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Type</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {experiments.map(experiment => {
+                            return <tr key={experiment.id}>
+                                <td>{experiment.name}</td>
+                                <td>{experiment.type}</td>
+                                <td>{experiment.status}</td>
+                                <td>
+                                    <button onClick={(e) => onExperimentStateChange(experiment, e.target.textContent === "start" ? "running" : "paused")}>{experiment.status === "running" ? "pause" : "start"}</button>
+                                </td>
+                            </tr>
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
