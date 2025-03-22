a7a3bb64c0fe1f15daff744e46c3c82991fbdbc8
MichaelSwain <michaeljswain@gmail.com>
2025-03-22 12:51:53 +0100

    move Modal styles to separate css file

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,29 +1,32 @@
+import './Modal.css';
+
 export const Modal = ({children, closeFunc, submitFunc, header, cta}) => {
     return (
-        <div style={{position: 'fixed', height: '100vh', width: '100vw'}}>
-                    <div className='modal-overlay' style={{ background: 'rgba(0,0,0,.5)', position: 'absolute', zIndex: '-1', height: '100%', width: '100%'}}></div>
-                    <div className='modal' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
-<div style={{width: 'fit-content', height: 'fit-content', padding: '20px', background: 'lightgrey', borderRadius: '5px'}}>
+        <div className="modal-container">
+            <div className='modal-overlay'></div>
+            <div className='modal'>
+                <div className='modal-content'>
                     <form onSubmit={(e) => {e.preventDefault(); submitFunc(e); closeFunc(false);}}>
-                        <div className="modal-header" style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <div className="modal-header">
                             <div>
-                                <h4 className="modal-title">{header}</h4>
+                                <h4 className="modal-title">
+                                    {header}
+                                </h4>
                             </div>
-                            <button style={{background: 'none'}} onClick={closeFunc}>X</button>
+                            <button className='modal-close-btn' onClick={closeFunc}>X</button>
                         </div>
                         
-                        <div className="modal-content">
+                        <div className="modal-body">
                             {children}
                         </div>
                         
-                        <div className="modal-footer" style={{marginTop: '15px', display: 'flex', justifyContent: 'flex-end'}}>
-                            <button onClick={closeFunc} style={{background: 'none', marginRight: '15px'}}>Cancel</button>
+                        <div className="modal-footer">
+                            <button onClick={closeFunc} className='modal-cancel-btn'>Cancel</button>
                             <button type="submit">{cta}</button>
                         </div>
-
                     </form>
                 </div>
-                </div>
-                </div>
+            </div>
+        </div>
     )
 }
