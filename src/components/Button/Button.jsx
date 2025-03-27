8c5a91e4b8c966955fbbbe0565bab65640abac29
Michael Swain <michaelswain@tommy.com>
2025-03-27 15:15:38 +0100

    create GridLayout and GridLayoutItem components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import './Button.css'
 
-export const Button = ({children, ariaLabel, type = "button", isDisabled, isLink, isLoading, isSubmit, loadingText, onClick, style, width, size}) => {
-    const classList = `ui-button ui-button-${style} ${size ? `ui-button-${size}` : ''} ${width ? `ui-button-${width}` : ''}`;
+export const Button = ({children, className, ariaLabel, type = "button", isDisabled, isLink, isLoading, isSubmit, loadingText, onClick, style, width, size}) => {
+    const classList =`ui-button ui-button-${style} ${size ? `ui-button-${size}` : ''} ${width ? `ui-button-${width}` : ''} ${className ? className : ''}`;
 
     return (
         <button className={classList} type={type} onClick={onClick}>{children}</button>
