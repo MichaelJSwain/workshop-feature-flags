b953f8380ffb594eedaf8cd1c4aa5f9646772d40
Michael Swain <michaelswain@tommy.com>
2025-05-09 13:35:45 +0200

    wrap list of rules cards in container

--- src/pages/FlagDetailView/FlagDetailView.jsx
+++ src/pages/FlagDetailView/FlagDetailView.jsx
@@ -62,60 +62,69 @@ export const FlagDetailView = () => {
 
                 {(!isLoading && flag) && 
                     <>
-                    <button onClick={() => handleRuleFormTrigger()}>Add Rule</button>
+                    
                      {isShowingRuleForm && <RuleForm initialValues={selectedRule || emptyRule} submitFunc={selectedRule ? handleUpdateRule : handleAddRule}></RuleForm>}
-
-                    { flag.rulesConfigs && flag.rulesConfigs.map(ruleConfig => {
-                        return <div key={Math.random() * 10000} style={{borderRadius: "8px", border: "1px solid gray", marginBottom: "20px", padding: "30px"}}>
-                                <div style={{display: "flex", justifyContent: "space-between"}}>
-                                    <div style={{display: "flex", gap: "10px"}}>
-                                        <h2 style={{margin: "0px"}}>Rule: {ruleConfig.name}</h2>
-                                        <div style={{background: ruleConfig.status === "paused" ? "pink" : "green", color: "white", fontWeight: "bold", padding: "5px 10px", borderRadius: "25px", display: "flex", alignItems: "center"}}>
-                                            {ruleConfig.status}
+                     <div style={{border: "1px solid black", padding: "20px 30px"}}>
+                    <div style={{display: "flex", justifyContent: "space-between", marginBottom: "20px"}}>
+                        <h2 style={{textAlign: "left", margin: "0px"}}>Rules</h2>
+                        <button onClick={() => handleRuleFormTrigger()}>Add Rule</button>
+                    </div>
+                    <div>
+                        
+                        { (flag.rulesConfigs && flag.rulesConfigs.length) ? flag.rulesConfigs.map(ruleConfig => {
+                            return <div key={Math.random() * 10000} style={{borderRadius: "8px", border: "1px solid gray", marginBottom: "20px", padding: "30px"}}>
+                                    <div style={{display: "flex", justifyContent: "space-between"}}>
+                                        <div style={{display: "flex", gap: "10px"}}>
+                                            <h2 style={{margin: "0px"}}>Rule: {ruleConfig.name}</h2>
+                                            <div style={{background: ruleConfig.status === "paused" ? "pink" : "green", color: "white", fontWeight: "bold", padding: "5px 10px", borderRadius: "25px", display: "flex", alignItems: "center"}}>
+                                                {ruleConfig.status}
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <button type="button" onClick={() => {handleRuleFormTrigger(ruleConfig)}} style={{marginRight: "10px"}}>Update</button>
+                                            <button type="button" onClick={() => handleDeleteRule(ruleConfig.id)}>Delete</button>
                                         </div>
                                     </div>
-                                    <div>
-                                        <button type="button" onClick={() => {handleRuleFormTrigger(ruleConfig)}}>Update</button>
-                                        <button type="button" onClick={() => handleDeleteRule(ruleConfig.id)}>Delete</button>
+                                    <div style={{display: "flex"}}>
+                                        <div>
+                                            Key:
+                                        </div>
+                                        <div>
+                                            {ruleConfig.key}
+                                        </div>
                                     </div>
-                                </div>
-                                <div style={{display: "flex"}}>
-                                    <div>
-                                        Key:
+                                    <div style={{display: "flex"}}>
+                                        <div>
+                                            Traffic Allocation:
+                                        </div>
+                                        <div>
+                                            {ruleConfig.percentage_included}
+                                            %
+                                        </div>
                                     </div>
-                                    <div>
-                                        {ruleConfig.key}
-                                    </div>
-                                </div>
-                                <div style={{display: "flex"}}>
-                                    <div>
-                                        Traffic Allocation:
-                                    </div>
-                                    <div>
-                                        {ruleConfig.percentage_included}
-                                        %
-                                    </div>
-                                </div>
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <td>Variation:</td>
-                                            <td>Distribution:</td>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    {ruleConfig.variations.map(v => {
-                                        return ( 
-                                            <tr key={v.variation_id}>
-                                                <td>{v.name}</td>
-                                                <td>{v.percentage_included}%</td>
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <td>Variation:</td>
+                                                <td>Distribution:</td>
                                             </tr>
-                                        )
-                                    })}
-                                    </tbody>
-                                </table>
-                            </div>
-                    })}
+                                        </thead>
+                                        <tbody>
+                                        {ruleConfig.variations.map(v => {
+                                            return ( 
+                                                <tr key={v.variation_id}>
+                                                    <td>{v.name}</td>
+                                                    <td>{v.percentage_included}%</td>
+                                                </tr>
+                                            )
+                                        })}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            
+                        }) : <p>no rules have been added yet</p>}
+                    </div>
+                    </div>
                     </>
                 }
 
