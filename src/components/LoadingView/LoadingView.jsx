24d7024851ef7c47f9e586dd6b453855b08ee650
Michael Swain <michaelswain@tommy.com>
2025-05-11 11:09:51 +0200

    create LoadingView component and render on pages

---
+++ src/components/LoadingView/LoadingView.jsx
@@ -0,0 +1,9 @@
+import './LoadingView.css'
+
+export const LoadingView = () => {
+    return (
+        <div className='loading-view-container'>
+            <h3>Loading...</h3>
+        </div>
+    )
+}
